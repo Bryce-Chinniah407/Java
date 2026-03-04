f3af95960787c2b3f1f69501af680cf17c10d3da
Bryce-Chinniah407 <Bryce.Chinniah@gmail.com>
2026-03-04 17:11:04 +0400

    interface and error handling

---
+++ Interface_error-handling/inter_multi.java
@@ -0,0 +1,20 @@
+interface MyInterface {
+    public void method1();
+    public void method2();
+}
+
+public class inter_multi implements MyInterface {
+    public void method1(){
+        System.out.println("Implementation of method1");
+    }
+
+    public void method2() {
+        /*System.out.println("Implementation of method2");*/
+    }
+
+    public static void main(String[] args) {
+        MyInterface obj = new inter_multi();
+        obj.method1();
+        obj.method2();
+    }
+}
